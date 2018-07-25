76ecc2060b84bb2cc1671e05a523e632d8d488b9
Chris Hamer-Hodges <chris.hhodges@gmail.com>
2018-07-25 21:01:04 +0100

    Unit tests for strava token

--- test/strava_token.test.js
+++ test/strava_token.test.js
@@ -2,10 +2,16 @@ const chai = require('chai');
 const expect = chai.expect;
 
 // Import the JS to test
-const strava_token = require('../lib/strava_token');
+import strava_token from '../lib/strava_token';
 
 describe('Check Strava Token is set', function() {
   it('should be defined', function() {
-    expect(strava_token).to.not.be(undefined);
+    expect(strava_token).to.not.equal(undefined);
+  });
+  it('should not be default value', function() {
+    expect(strava_token).to.not.equal('TOKEN_GOES_HERE');
+  });
+  it('should have correct length', function() {
+    expect(strava_token).to.have.lengthOf(40);
   });
 });
