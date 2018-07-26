39f259fec7360f7072b73f344bad711f7e90564b
Chris Hamer-Hodges <chris.hhodges@gmail.com>
2018-07-26 22:14:55 +0100

    Complete test for getAthlete

--- test/getAthlete.test.js
+++ test/getAthlete.test.js
@@ -1,8 +1,21 @@
+const chai = require('chai');
+const expect = chai.expect;
+
 import { getAthlete } from '../lib/strava';
 
 describe('getAthlete', function() {
   it('should load', function(done) {
+    this.slow(800);
     getAthlete()
-      .then(done());
+      .then(() => done())
+      .catch(done);
+  });
+  it('should contain expected data', async function() {
+    this.slow(800);
+    const data = await getAthlete();
+    expect(data).to.have.property('id');
+    expect(data).to.have.property('username');
+    expect(data).to.have.property('firstname');
+    expect(data).to.have.property('lastname');
   });
 });
