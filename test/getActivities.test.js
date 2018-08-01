df97cdfb0d34dc02d9ffea4ced7cda3dad15da78
Chris Hamer-Hodges <chris.hamer-hodges@autodesk.com>
2018-08-01 16:19:50 +0100

    Complete test for get activities

--- test/getActivities.test.js
+++ test/getActivities.test.js
@@ -30,24 +30,48 @@ describe('getActivities', function() {
   });
 });
 
-it('before and after should work', async function() {
-  this.timeout(5000);
-  this.slow(800);
-  // Get activities from June 2018
-  let before = new Date(2018, 6, 1);
-  let after = new Date(2018, 5, 1);
-  const data = await getActivities({before,after});
-  const inRange = function(entry) {
-    const start = new Date(entry.start_date);
-    return start < before && start > after;
-  };
-  expect(data).to.be.an('array');
-  expect(data.every(inRange)).to.be.true;
+describe('options', function() {
+  it('before and after should work', async function() {
+    this.timeout(5000);
+    this.slow(800);
+    // Get activities from June 2018
+    let before = new Date(2018, 6, 1);
+    let after = new Date(2018, 5, 1);
+    const data = await getActivities({before,after});
+    const inRange = function(entry) {
+      const start = new Date(entry.start_date);
+      return start < before && start > after;
+    };
+    expect(data).to.be.an('array');
+    expect(data.every(inRange)).to.be.true;
+  });
+  
+  it('per_page should work', async function() {
+    this.timeout(5000);
+    this.slow(800);
+  
+    const per_page = 7;
+    const data = await getActivities({per_page});
+    expect(data).to.be.an('array');
+    expect(data.length).to.equal(per_page);
+  });
+
+  it('page should work', async function() {
+    this.timeout(5000);
+    this.slow(800);
+  
+    const standard = await getActivities();
+    const page1 = await getActivities({page: 1});
+    const page2 = await getActivities({page: 2});
+    expect(page1[0]).to.deep.equal(standard[0]);
+    expect(page2[0]).to.not.deep.equal(page1[0]);
+  });
+
 });
 
 describe('caching', function() {
   it('should not make https request more than once', function() {
-    expect(https.request.callCount).to.be.at.most(1);
+    expect(https.request.callCount).to.be.at.most(5);
   });
 
   after(function() {
