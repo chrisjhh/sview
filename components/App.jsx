23f8341aabcb6d202ea828a62934868380e5ed04
Chris Hamer-Hodges <chris.hamer-hodges@autodesk.com>
2018-08-01 17:07:21 +0100

    Start react work

---
+++ components/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () => (
+  <h1>It&apos;s me again!</h1>
+);
+
+export default App;
---
+++ index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import RenderDOM from 'react-dom';
+import App from './components/App';
+
+RenderDOM.render(
+  <App/>,
+  document.getElementById('app')
+);
