a4cddba00d96c7b253375d08485dc9e57888fd7d
Chris Hamer-Hodges <chris.hamer-hodges@autodesk.com>
2018-08-06 14:30:35 +0100

    Run duration

--- components/Run.jsx
+++ components/Run.jsx
@@ -4,7 +4,19 @@ import PropTypes from 'prop-types';
 const miles = function(distance) {
   let mi = Number(distance) / 1609.34;
   return mi.toFixed(1).toString() + 'mi';
-}
+};
+
+const duration = function(time) {
+  let mins = Math.floor(time/60);
+  let secs = time - mins * 60;
+  let hours = 0;
+  if (mins > 60) {
+    hours = Math.floor(mins/60);
+    mins = mins - 60 * hours;
+    return `${hours}:${mins.toString().padStart(2,'0')}:${secs.toString().padStart(2,'0')}`;
+  }
+  return `${mins}:${secs.toString().padStart(2,'0')}`;
+};
 
 const Run = props => (
   <div className="activity run">
@@ -14,7 +26,10 @@ const Run = props => (
         <span className='title'>{props.activity.name}</span>
       </div>
       <div className="row2">
-        <span className='distance'>{miles(props.activity.distance)}</span>
+        <span className='detail distance'>{miles(props.activity.distance)}</span>
+        <span className='detail duration'>{
+          duration(props.activity.workout_type === 1 ? 
+            props.activity.elapsed_time : props.activity.moving_time)}</span>
       </div>
     </div>
   </div>
