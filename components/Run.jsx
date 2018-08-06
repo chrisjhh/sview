792540b21d370acb160af5f4082cdc2adf46da2a
Chris Hamer-Hodges <chris.hamer-hodges@autodesk.com>
2018-08-06 14:08:11 +0100

    Add distances

--- components/Run.jsx
+++ components/Run.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const miles = function(distance) {
+  let mi = Number(distance) / 1609.34;
+  return mi.toFixed(1).toString() + 'mi';
+}
+
 const Run = props => (
   <div className="activity run">
     <div className={props.activity.workout_type === 1 ? 'type race' : 'type'}></div>
     <div className="contents">
-      <span className='title'>{props.activity.name}</span>
+      <div className="row1">
+        <span className='title'>{props.activity.name}</span>
+      </div>
+      <div className="row2">
+        <span className='distance'>{miles(props.activity.distance)}</span>
+      </div>
     </div>
   </div>
 );
--- components/Swim.jsx
+++ components/Swim.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const metres = function(distance) {
+  return Number(distance).toFixed(0).toString() +  'm';
+};
+
 const Swim = props => (
   <div className="activity swim">
     <div className="type"></div>
     <div className="contents">
-      <span className='title'>{props.activity.name}</span>
+      <div className="row1">
+        <span className='title'>{props.activity.name}</span>
+      </div>
+      <div className="row2">
+        <span className='distance'>{metres(props.activity.distance)}</span>
+      </div>
     </div>
   </div>
 );
