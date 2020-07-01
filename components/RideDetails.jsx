2027bc9e218be76ad26addd9563b1ce0dbbedb9b
Chris Hamer-Hodges <chris.hamer-hodges@autodesk.com>
2020-07-01 16:32:18 +0100

    Calculate distance and velocity for exercise bike activities

--- components/RideDetails.jsx
+++ components/RideDetails.jsx
@@ -10,10 +10,32 @@ const miles = function(distance) {
   return mi.toFixed(1).toString() + 'mi';
 };
 
+const distance = function(activity) {
+  const a_dist = activity.distance;
+  const duration = activity.elapsed_time;
+  if (!a_dist || a_dist < duration) {
+    // No movement or less than 1m/s likely to be exercise bike!
+    const rpm = activity.average_cadence;
+    const revolutions = rpm * duration / 60;
+    const c_dist = revolutions * 4.48; // <- HARDCODED value TODO: Replace
+    return c_dist;
+  }
+  return distance;
+};
+
+const speed = function(activity) {
+  const miles = distance(activity)/1609.34;
+  const hours = activity.elapsed_time / 3600;
+  return (miles/hours).toFixed(1).toString();
+}
+
 const RideDetails = props => (
   <span className='detail'>
-    <span className='distance'>{miles(props.activity.distance)}</span>
+    <span className='distance'>{miles(distance(props.activity))}</span>
     <span className='duration'>{duration(props.activity.elapsed_time)}</span>
+    <span className='speed'>{speed(props.activity)}
+      <span className="units">mph</span>
+    </span>
     <HeartRate  activity={props.activity}/>
     <XPoints activity={props.activity}/>
     <Cadence activity={props.activity}/>
